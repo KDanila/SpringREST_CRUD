e54b46416fbb70a6fb6c2acbd70786be5bbdde8a
KDanila <concista@gmail.com>
2020-04-10 10:40:39 +0300

    Add link and controller.

--- src/main/java/kdv/controller/CustomerRestController.java
+++ src/main/java/kdv/controller/CustomerRestController.java
@@ -4,6 +4,7 @@ import kdv.entity.Customer;
 import kdv.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId){
+        return customerService.getCustomer(customerId);
     }
 }
